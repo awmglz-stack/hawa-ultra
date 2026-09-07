ab05e39e8f0471379c4c577f12bb02290d3bfee3
awmglz-stack <awmglz@gmail.com>
2026-09-07 16:51:02 +0300

    Add home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>مرحباً بك في Hawa Ultra 🚀</h1>
+      <p>تطبيق Next.js حديث وقوي</p>
+    </main>
+  )
+}
